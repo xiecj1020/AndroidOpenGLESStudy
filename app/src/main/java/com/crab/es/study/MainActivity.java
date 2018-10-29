fb2b33f0949618f08b09d82cadd40cadf1551a32
crabisacoolboy <crabisacoolboy@qq.com>
2018-10-29 17:41:35 +0800

    Title:Add a FrameBufferObject demo
    Summary:none

--- app/src/main/java/com/crab/es/study/MainActivity.java
+++ app/src/main/java/com/crab/es/study/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Intent intent = new Intent(MainActivity.this,VrContextActivity.class);
+                Intent intent = new Intent(MainActivity.this,FrameBufferObjectActivity.class);
                 startActivity(intent);
             }
         });
